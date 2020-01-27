0ded7504c63d53dd50651cb8c221ae2674b67ac3
Moshiur1304097 <moshiurrahma6674@gmail.com>
2020-01-28 00:36:38 +0600

    Compononent_create_and_api_service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,42 @@
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductsComponent } from './products/products.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductAddComponent } from './product-add/product-add.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+          {
+            path: 'products',
+            component: ProductsComponent,
+            data: { title: 'List of Products'}
+          },
+
+          {
+            path: 'product-details/:id',
+            component: ProductDetailComponent,
+            data: { title: 'Product Details'}
+          },
+
+          {
+            path: 'product-add',
+            component: ProductAddComponent,
+            data: {title: 'Add Product'}
+          },
+
+          {
+            path: 'product-edit',
+            component: ProductEditComponent,
+            data: { title: 'Edit Product'}
+          },
+
+          {
+            path: '',
+            redirectTo:'/products',
+            pathMatch: 'full'
+          }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
